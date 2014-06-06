a8ee309f3000c0618864c9fb0b67a62285e66238
Max <maksold@gmail.com>
2014-06-06 17:45:45 +0300

    Update SpeedsterMinify.php
    Added compression for long URL

--- lib/minify/SpeedsterMinify.php
+++ lib/minify/SpeedsterMinify.php
@@ -2,5 +2,27 @@
 
 class SpeedsterMinify extends Minify
 {
+    static public $encodeReplace = array('/', '+');
+    static public $decodeReplace = array('-', '_');
 
+    /**
+     * @param $str
+     * @return string
+     */
+    static public function encodeMinURL($str)
+    {
+        return str_replace(self::$encodeReplace, self::$decodeReplace, base64_encode(gzdeflate($str, 9)));
+    }
+
+    /**
+     * @param $str
+     * @return string
+     */
+    static public function decodeMinURL($str)
+    {
+        if (strpos($str, '/') === 0) {
+            $str = substr($str, 1);
+        }
+        return gzinflate(base64_decode(str_replace(self::$decodeReplace, self::$encodeReplace, $str)));
+    }
 }
